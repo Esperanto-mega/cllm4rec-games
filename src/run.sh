13d3c7592fae9803430e7ddb4fe11350ae67fba6
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-08-20 09:40:23 +0800

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lambda_V=1
-dataset=beauty
+dataset=Beauty
 
 accelerate launch --multi_gpu --num_processes=8 src/training.py --dataset $dataset --lambda_V $lambda_V
 accelerate launch --multi_gpu --num_processes=8 src/finetuning.py --dataset $dataset --lambda_V $lambda_V
