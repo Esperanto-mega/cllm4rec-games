441cb2cef346b3f97caccb3f1a2e7cd31b0ddfb0
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-08-20 10:11:42 +0800

    Update finetuning.py

--- src/finetuning.py
+++ src/finetuning.py
@@ -63,7 +63,7 @@ def save_remote(local_path, remote_path, local_mode, remote_mode):
     with fsspec.open(remote_path, remote_mode) as f:
         f.write(content)
 
-server_root = "/datain/v-yinju/rqvae-zzx/models/cllm4rec/Beauty"
+server_root = "/datain/v-yinju/rqvae-zzx/models/cllm4rec"
 local_root = "tmp"
 if not os.path.exists(local_root):
     os.makedirs(local_root, exist_ok=True)
