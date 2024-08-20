eb956d51f5dd26b40e3446b3aee51986dda00d68
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2024-08-20 10:21:06 +0800

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -4,5 +4,11 @@ lambda_V=1
 dataset=Beauty
 
 accelerate launch --multi_gpu --num_processes=8 ~/cllm4rec-games/src/training.py --dataset $dataset --lambda_V $lambda_V
-accelerate launch --multi_gpu --num_processes=8 ~/cllm4rec-games/src/finetuning.py --dataset $dataset --lambda_V $lambda_V
+
+accelerate launch --multi_gpu --num_processes=8 ~/cllm4rec-games/src/finetuning.py \
+  --dataset $dataset \
+  --lambda_V $lambda_V \
+  --batch_size 8 \
+  --val_batch_size 256
+
 python ~/cllm4rec-games/src/predict.py --dataset $dataset --lambda_V $lambda_V
